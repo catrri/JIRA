42ab66a7660a4448ded7f1c112205fb557c973dc
catrri <catrri@gmail.com>
2019-10-06 19:58:56 +0300

    CreateIssue

--- src/CreateIssue.java
+++ src/CreateIssue.java
@@ -1,9 +1,9 @@
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
 import static org.testng.Assert.assertTrue;
 
 public class CreateIssue {
@@ -30,24 +30,28 @@ public class CreateIssue {
     String userName = "Ekaterina_Voronkova";
     String password = "Ekaterina_Voronkova";
 
+
     @Test
     public void firstTest() throws InterruptedException {
         this.driver.get("https://jira.hillel.it/login.jsp");
         this.driver.findElement(userNameInput).sendKeys(userName);
         this.driver.findElement(passwordInput).sendKeys(password);
         this.driver.findElement(enterButton).click();
-        Thread.sleep(2000);
+        Thread.sleep(3000);
+
+        Assert.assertEquals(driver.getCurrentUrl(), "https://jira.hillel.it/secure/Dashboard.jspa");
+
         this.driver.findElement(createButton).click();
         Thread.sleep(2000);
-        this.driver.findElement(projectInput);
-        this.driver.findElement(issueTypeInput);
-        this.driver.findElement(summaryInput).sendKeys("Test CreateIssue");
+        // this.driver.findElement(projectInput);
+        // this.driver.findElement(issueTypeInput);
+        this.driver.findElement(summaryInput).sendKeys("Test Create Issue");
         Thread.sleep(2000);
         this.driver.findElement(By.xpath("//*[@id=\"description-wiki-edit\"]")).click();
         this.driver.findElement(descriptionInput).sendKeys("Description of my Test Issue");
+        Thread.sleep(2000);
         this.driver.findElement(createIssue).click();
 
-        // Thread.sleep(5000);
         assertTrue(this.driver.findElement(createSuccsess).isDisplayed());
         Thread.sleep(5000);
 
